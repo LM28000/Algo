5ea6568c6e7f793e187df1869b0d1787d76042c8
Louis-Marie PERRET DU CRAY <louis-marie@du-cray.com>
2024-12-16 13:48:06 +0100

    v22

--- Algo/Program.cs
+++ Algo/Program.cs
@@ -7,27 +7,30 @@ namespace Test
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
+            // Déclaration des variables
             string langue;
-            int nbJoueur=2;
-            int tailleGrid=4;
+            int nbJoueur = 2;
+            int tailleGrid = 4;
 
+            // Demande à l'utilisateur de choisir une langue
             Console.WriteLine("Choose your language / Choix de langue : ");
             Console.WriteLine("- Francais");
             Console.WriteLine("- English");
             langue = Convert.ToString(Console.ReadLine());
+
+            // Boucle pour s'assurer que l'utilisateur entre une langue correcte
             while (langue != "Francais" && langue != "English")
             {
                 Console.WriteLine("Please enter a correct language / Veuillez entrer une langue correct");
                 langue = Convert.ToString(Console.ReadLine());
-            } 
+            }
 
-
+            // Création d'un nouvel objet Jeu avec la langue choisie
             Jeu Game1 = new Jeu(langue);
+            // Lancement du jeu
             Game1.LancerJeu(langue);
-            
         }
     }
 }
