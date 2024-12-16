126068a74402e11bb0059c90d6a5b5da974d115a
Louis-Marie PERRET DU CRAY <louis-marie@du-cray.com>
2024-12-16 08:55:46 +0100

    v2

--- Algo/Dictionnaire.cs
+++ Algo/Dictionnaire.cs
@@ -5,64 +5,66 @@ using System.Linq;
 
 namespace Algo
 {
-
-
-    private List<string> LireDictionnaire(string chemin)
+    class Dictionnaire
     {
-        var contenu = File.ReadAllText(chemin);
-        return contenu.Split(new[] { ' ', '\n', '\r' })
-                      .ToList();
-    }
 
-    private List<string> TriDictionnairefusion(List<string> dictionnaire)
-    {
-        if (dictionnaire.Count <= 1)
-            return dictionnaire;
-        List<string> gauche = new List<string>();
-        List<string> droite = new List<string>();
-        int milieu = dictionnaire.Count / 2;
-        for (int i = 0; i < milieu; i++)
+        private List<string> LireDictionnaire(string chemin)
         {
-            gauche.Add(dictionnaire[i]);
+            var contenu = File.ReadAllText(chemin);
+            return contenu.Split(new[] { ' ', '\n', '\r' })
+                          .ToList();
         }
-        for (int i = milieu; i < dictionnaire.Count; i++)
-        {
-            droite.Add(dictionnaire[i]);
-        }
-        gauche = TriDictionnairefusion(gauche);
-        droite = TriDictionnairefusion(droite);
-        return Fusionner(gauche, droite);
-    }
 
-    private List<string> Fusionner(List<string> gauche, List<string> droite)
-    {
-        List<string> resultat = new List<string>();
-        while (gauche.Count > 0 || droite.Count > 0)
+        private List<string> TriDictionnairefusion(List<string> dictionnaire)
         {
-            if (gauche.Count > 0 && droite.Count > 0)
+            if (dictionnaire.Count <= 1)
+                return dictionnaire;
+            List<string> gauche = new List<string>();
+            List<string> droite = new List<string>();
+            int milieu = dictionnaire.Count / 2;
+            for (int i = 0; i < milieu; i++)
             {
-                if (gauche[0].CompareTo(droite[0]) < 0)
+                gauche.Add(dictionnaire[i]);
+            }
+            for (int i = milieu; i < dictionnaire.Count; i++)
+            {
+                droite.Add(dictionnaire[i]);
+            }
+            gauche = TriDictionnairefusion(gauche);
+            droite = TriDictionnairefusion(droite);
+            return Fusionner(gauche, droite);
+        }
+
+        private List<string> Fusionner(List<string> gauche, List<string> droite)
+        {
+            List<string> resultat = new List<string>();
+            while (gauche.Count > 0 || droite.Count > 0)
+            {
+                if (gauche.Count > 0 && droite.Count > 0)
+                {
+                    if (gauche[0].CompareTo(droite[0]) < 0)
+                    {
+                        resultat.Add(gauche[0]);
+                        gauche.Remove(gauche[0]);
+                    }
+                    else
+                    {
+                        resultat.Add(droite[0]);
+                        droite.Remove(droite[0]);
+                    }
+                }
+                else if (gauche.Count > 0)
                 {
                     resultat.Add(gauche[0]);
                     gauche.Remove(gauche[0]);
                 }
-                else
+                else if (droite.Count > 0)
                 {
                     resultat.Add(droite[0]);
                     droite.Remove(droite[0]);
                 }
             }
-            else if (gauche.Count > 0)
-            {
-                resultat.Add(gauche[0]);
-                gauche.Remove(gauche[0]);
-            }
-            else if (droite.Count > 0)
-            {
-                resultat.Add(droite[0]);
-                droite.Remove(droite[0]);
-            }
+            return resultat;
         }
-        return resultat;
     }
 }
